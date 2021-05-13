bcecb5dacbabd997e6215623a099546bbdb636cf
hayabusa1601 <1601hayabusa@gmail.com>
2021-05-13 16:51:58 +0900

    [Add] 5/11の授業課題

---
+++ 2021TE3/procodes/problem2021011.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+struct bits {
+  unsigned int b1 : 1;
+  unsigned int b2 : 1;
+  unsigned int b3 : 1;
+  unsigned int b4 : 1;
+  unsigned int b5 : 1;
+  unsigned int b6 : 1;
+  unsigned int b7 : 1;
+  unsigned int b8 : 1;
+};
+
+typedef union {
+  unsigned char ch;
+  struct bits b;
+}u;
+
+int main(void) {
+  u moji;
+  moji.ch = 'a';
+  printf("%cのビット表示は",moji.ch);
+  printf("%d%d%d%d%d%d%d%d", moji.b.b1, moji.b.b2, moji.b.b3, moji.b.b4, moji.b.b5, moji.b.b6, moji.b.b7, moji.b.b8);
+
+  printf("です\n");
+
+  return 0;
+}
+
