4fae5162f7e71afe94280866507eb238517a432f
hayabusa1601 <1601hayabusa@gmail.com>
2021-07-07 08:13:38 +0900

    [Add] 6/29の授業課題

---
+++ 2021TE3/problem2021018.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define N 20000
+
+void swap (int *x, int *y) {
+  int tmp = *x;
+  *x = *y;
+  *y = tmp;
+}
+
+/*バブルソート*/
+/*void bsort(int *a) {
+   int i,j;
+   for(i = 0; i < N - 1; i++) {
+     for(j = 0; j < N - i - 1; j++) {
+       if(a[j] > a[j+1]) {
+         swap(&a[j], &a[j + 1]);
+     }
+}
+}
+}
+*/
+
+/*挿入ソート*/
+void insort(int *a) {
+  int i,j;
+
+  for(i = 1; i < N; i++) {
+      j = i;//交換要素を探すインデックスj
+
+      //整列済みの場合処理しない
+      while((j > 0) && (a[j - 1] > a[j])) {
+        swap(&a[j - 1], &a[j]);//整列されていなければ隣り合う要素を交換
+
+        //隣り合う要素のインデックスを更新
+        j--;
+
+   }
+  }
+}
+
+
+int main(void) {
+  clock_t t_start, t_end;
+  t_start = clock();//タイム計測の準備
+
+  srand((unsigned int)time(NULL));//乱数のタネ
+
+  int a[N];
+  int i;
+
+  for(i = 0; i < N; i++) {
+    a[i] = rand();
+  }//乱数を代入
+
+  insort(a);//insort関数に渡してinsert sort
+
+  for(i = 0; i < N; i++) {
+    printf("%d\n", a[i]);
+  }
+
+  t_end = clock();//タイム計測の終了
+  printf("実行時間:%f\n",(double)(t_end - t_start)/CLOCKS_PER_SEC);
+  return 0;
+}
